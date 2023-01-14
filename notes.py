e72eaabcad4ab8dbcbdf8ca6da8380dab2dc6826
Shreya7077 <shreya.guthikonda@gmail.com>
2023-01-14 22:26:37 +0530

    second commit

---
+++ notes.py
@@ -0,0 +1,16 @@
+first time upload 
+
+git init  
+git add . 
+git commit -m 'first commit'
+git remote add origin link  of repo 
+git branch -M master 
+git push origin master  
+
+git status 
+
+
+for modifications 
+git add . 
+git commit -m 'second commit'
+git push 
